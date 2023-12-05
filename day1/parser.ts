ebd7e29ffd5a65dee7055d031ba85dc343246703
AnsgarLichter <lichteransgar@gmail.com>
2023-12-05 17:04:49 +0100

    feat: day01 part 1

--- day1/parser.ts
+++ day1/parser.ts
@@ -2,8 +2,8 @@ import { match } from "assert";
 import { BunFile } from "bun";
 
 export class Parser {
-    private regexFirstNumber = new RegExp(/(one|1|two|2|three|3|four|4|five|5|six|6|seven|7|eight|8|nine|9).*/);
-    private regexLastNumber = new RegExp(/.*(one|1|two|2|three|3|four|4|five|5|six|6|seven|7|eight|8|nine|9).*/);
+    private regexFirstNumber = new RegExp(/(1|2|3|4|5|6|7|8|9).*/);
+    private regexLastNumber = new RegExp(/.*(1|2|3|4|5|6|7|8|9).*/);
 
     public solve(calibrationInput: String[]): Number[] {
         const result: Number[] = new Array();
@@ -15,10 +15,6 @@ export class Parser {
 
             if (!firstNumber || !lastNumber) {
                 console.log(`Couldn't parse line ${lineAsString} correctly. Found first number ${firstNumber} and last number ${lastNumber}`);
-            }
-
-            if (line === "3one8ncctmbsixeighttwonegb" || line === "jgtwone321fourkjg3") {
-                debugger;
             }
 
             const number = Number.parseInt(`${firstNumber}${lastNumber}`);
